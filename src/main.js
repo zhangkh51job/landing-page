5292a16ac4fc4e20373fe00a278e39bf4559cc2f
zhangkh51job <zhangkh51job@gmail.com>
2023-01-17 17:30:42 +0400

    11

--- src/main.js
+++ src/main.js
@@ -3,25 +3,25 @@ import App from './App.vue'
 import router from './router'
 import 'amfe-flexible/index.js'
 import "./utils/pcRem"
-import { Dialog, Popup, Swipe, SwipeItem, Toast, Button,/*, Tab, Tabs,ActionBar, ActionBarIcon, ActionBarButton, Divider, Overlay, Loading,  ContactCard, Form, AddressEdit, AddressList, Field, CellGroup, Cell, SwipeCell, Icon, Stepper, Card, Checkbox, CheckboxGroup,  PullRefresh, List,  SubmitBar,  Skeleton, Picker, ActionSheet*/ } from 'vant'
+import { Dialog, Popup, Swipe, SwipeItem, Toast, Button,Icon, /*, Tab, Tabs,ActionBar, ActionBarIcon, ActionBarButton, Divider, Overlay, Loading,  ContactCard, Form, AddressEdit, AddressList, Field, CellGroup, Cell, SwipeCell, Icon, Stepper, Card, Checkbox, CheckboxGroup,  PullRefresh, List,  SubmitBar,  Skeleton, Picker, ActionSheet*/ } from 'vant'
 import 'vant/lib/index.css'; // 全局引入样式
 import '@/assets/global.less'
 import 'animate.css'
-import {i18n} from './assets/lang'
-import store from './vuex'
+// import {i18n} from './assets/lang'
+// import store from './vuex'
 // import theme from './assets/theme'
 
-import Global from "./utils/Global"
-window.Global = Global
-Global.setup();
+// import Global from "./utils/Global"
+// window.Global = Global
+// Global.setup();
 
 const app = createApp(App)
-import SvgIcon from '@/components/SvgIcon'// svg component
-// import { useI18n } from "vue-i18n"
-// app.config.globalProperties = useI18n
-window.$$i18n = i18n
-console.log('i18n', i18n)
-import './components/icons'
+// import SvgIcon from '@/components/SvgIcon'// svg component
+// // import { useI18n } from "vue-i18n"
+// // app.config.globalProperties = useI18n
+// window.$$i18n = i18n
+// console.log('i18n', i18n)
+// import './components/icons'
 
 app.use(Popup)
   // .use(ActionBarButton)
@@ -32,6 +32,7 @@ app.use(Popup)
   // .use(Loading)
   .use(Dialog)
   .use(Toast)
+  .use(Icon)
   // .use(ContactCard)
   // .use(Form)
   // .use(AddressEdit)
@@ -59,9 +60,10 @@ app.use(Popup)
 
 
 // register globally
-app.component('svg-icon', SvgIcon)
+// app.component('svg-icon', SvgIcon)
 
-app.use(i18n).use(store)
+// app.use(i18n)
+// app.use(store)
 app.use(router).mount('#app')
 
 
